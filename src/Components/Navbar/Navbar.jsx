c476ec95a3ccc186b6e3de95a8df3533a0686356
ronak-d <ronakdodeja73@gamil.com>
2023-04-06 22:22:43 +0530

    i

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,11 +8,6 @@ const Navbar = () => {
       <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="#" class="flex items-center">
-            <Image
-              src="https://flowbite.com/docs/images/logo.svg"
-              class="h-8 mr-3"
-              alt="Blogs"
-            />
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               Bello
             </span>
